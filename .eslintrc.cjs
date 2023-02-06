6f03de7e07434693eaa29108f317a23bb0e335a3
Lucas Demea <lucas@digital-swing.com>
2023-02-06 07:38:35 +0100

    test: replace formatting rules with prettier config
    as suggested here: https://typescript-eslint.io/rules/indent/

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,28 +1,24 @@
-const tsConfig = require("./tsconfig.json");
+const tsConfig = require('./tsconfig.json');
 // const tsTestConfig = require('./tsconfig.test.json');
 module.exports = {
-	parserOptions: {
-		ecmaVersion: 2018,
-		sourceType: "module",
-	},
-	rules: {
-		semi: 2,
-		indent: ["error", 2],
-	},
-	overrides: [
-		{
-			extends: [
-				"eslint:recommended",
-				"plugin:@typescript-eslint/recommended",
-				"plugin:@typescript-eslint/recommended-requiring-type-checking",
-			],
-			files: tsConfig.include,
-			parser: "@typescript-eslint/parser",
-			parserOptions: {
-				project: ["./tsconfig.json"], // Specify it only for TypeScript files
-			},
-			plugins: ["@typescript-eslint/eslint-plugin", "eslint-plugin-tsdoc"],
-		},
-	],
-	ignorePatterns: ["sample", "**/node_modules/*.[tj]s", "**/dist/**/*.[tj]s"],
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  overrides: [
+    {
+      extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/recommended-requiring-type-checking',
+      ],
+      files: tsConfig.include,
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        project: ['./tsconfig.json'],
+      },
+      plugins: ['@typescript-eslint/eslint-plugin', 'eslint-plugin-tsdoc'],
+    },
+  ],
+  ignorePatterns: ['sample', '**/node_modules/*.[tj]s', '**/dist/**/*.[tj]s'],
 };
