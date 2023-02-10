e5e5ba149075162545568eac20d27c5558f8636a
Lucas Demea <lucas@digital-swing.com>
2023-02-10 21:30:05 +0100

    build: move types in dist folder

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,26 +1,26 @@
-import dts from "rollup-plugin-dts";
-import typescript from "@rollup/plugin-typescript";
+import dts from 'rollup-plugin-dts';
+import typescript from '@rollup/plugin-typescript';
 
 export default [
-	{
-		input: "lib/index.ts",
-		output: [
-			{
-				file: `dist/cjs/index.cjs`,
-				format: "cjs",
-			},
-			{
-				file: `dist/esm/index.js`,
-				format: "esm",
-			},
-		],
-		external: ["mixme", "toposort"],
-		plugins: [typescript({ sourceMap: true })],
-	},
-	{
-		// path to your declaration files root
-		input: "lib/index.ts",
-		output: [{ dir: "types", format: "es" }],
-		plugins: [dts()],
-	},
+  {
+    input: 'lib/index.ts',
+    output: [
+      {
+        file: `dist/cjs/index.cjs`,
+        format: 'cjs',
+      },
+      {
+        file: `dist/esm/index.js`,
+        format: 'esm',
+      },
+    ],
+    external: ['mixme', 'toposort'],
+    plugins: [typescript({ sourceMap: true })],
+  },
+  {
+    // path to your declaration files root
+    input: 'lib/index.ts',
+    output: [{ dir: 'dist', format: 'es' }],
+    plugins: [dts()],
+  },
 ];
