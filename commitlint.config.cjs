b2759fc408e029dd75db5e7f31411ff66720d7ce
David Worms <david@adaltas.com>
2024-07-28 19:38:28 +0200

    build: commitlint configuration

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,5 +1,21 @@
 module.exports = {
   extends: [
     "@commitlint/config-conventional"
-  ]
+  ], 
+  rules: {
+    "type-enum": [
+      2,
+      "always",
+      [
+        "build",
+        "chore",
+        "docs",
+        "feat",
+        "fix",
+        "refactor",
+        "revert",
+        "test",
+      ],
+    ],
+  },
 };
