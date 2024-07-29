d36c759f5ae01aac5641d5368e784eb96e2e2b7b
David Worms <david@adaltas.com>
2024-07-29 10:34:24 +0200

    refactor: commitlint and rollup prettifier

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,21 +1,10 @@
 module.exports = {
-  extends: [
-    "@commitlint/config-conventional"
-  ], 
+  extends: ["@commitlint/config-conventional"],
   rules: {
     "type-enum": [
       2,
       "always",
-      [
-        "build",
-        "chore",
-        "docs",
-        "feat",
-        "fix",
-        "refactor",
-        "revert",
-        "test",
-      ],
+      ["build", "chore", "docs", "feat", "fix", "refactor", "revert", "test"],
     ],
   },
 };
--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,16 @@
-
-export default [{
-  input: 'lib/index.js',
-  output: [
-    {
-      file: `dist/cjs/index.cjs`,
-      format: 'cjs'
-    }, {
-      file: `dist/esm/index.js`,
-      format: 'esm'
-    },
-  ],
-  external: ['mixme', 'toposort']
-}];
+export default [
+  {
+    input: "lib/index.js",
+    output: [
+      {
+        file: `dist/cjs/index.cjs`,
+        format: "cjs",
+      },
+      {
+        file: `dist/esm/index.js`,
+        format: "esm",
+      },
+    ],
+    external: ["mixme", "toposort"],
+  },
+];
