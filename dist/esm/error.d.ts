a27ba2177a13ce1a5199b10e73b170a18be8f72a
David Worms <david@adaltas.com>
2024-09-05 21:23:59 +0200

    feat: build typescript types (fix #4 fix #5)

--- dist/esm/error.d.ts
+++
@@ -1,8 +0,0 @@
-declare class PlugableError extends Error {
-    code: string;
-    [index: string]: unknown;
-    constructor(code: string, message: string | (string | object)[], ...contexts: Record<string, unknown>[]);
-}
-export { PlugableError };
-declare const _default: (code: string, message: string | (string | object)[], ...contexts: Record<string, unknown>[]) => PlugableError;
-export default _default;
--- rollup.config.js
+++ rollup.config.js
@@ -1,16 +1,38 @@
+import terser from "@rollup/plugin-terser";
+import typescript from "rollup-plugin-typescript2";
+import { dts } from "rollup-plugin-dts";
+import commonjs from "@rollup/plugin-commonjs";
+import del from "rollup-plugin-delete";
+
 export default [
   {
-    input: "lib/index.js",
+    input: "src/index.ts",
     output: [
       {
-        file: `dist/cjs/index.cjs`,
+        dir: `dist/cjs`,
         format: "cjs",
+        sourcemap: true,
       },
       {
-        file: `dist/esm/index.js`,
+        dir: `dist/esm`,
         format: "esm",
+        sourcemap: true,
       },
     ],
     external: ["mixme", "toposort"],
+    plugins: [
+      del({ targets: "dist/*" }),
+      commonjs(),
+      typescript({
+        tsconfig: "tsconfig.src.json",
+        useTsconfigDeclarationDir: true,
+      }),
+      terser(),
+    ],
+  },
+  {
+    input: "./dist/types/index.d.ts",
+    output: [{ file: "dist/types/index.d.ts" }],
+    plugins: [dts(), del({ targets: "dist/types", hook: "buildEnd" })],
   },
 ];
